a9ef7caf468d8aae5e5db713d9a6878f4454e366
David M. Smith, Jr <dsmith@integralads.com>
2018-01-31 12:18:37 -0500

    delete dsmith if it exists before continuing

--- dsmith.sh
+++ dsmith.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git checkout -b dsmith
+function die {
+    >&2 echo "$1"
+    exit "$2"
+}
+
+git branch -D dsmith
+git checkout -b dsmith || die "failed to create dsmith" 1
 touch dsmith.txt
 git add dsmith.txt
 git commit -m "adding dsmith.txt"
